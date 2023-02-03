aea1dd0ee54c929f3b1fb74ceda51a43bc381d23
loumon koffi samuel <lkoffisamuel@gmail.com>
2023-02-03 15:02:09 +0000

    add Headliners section

--- src/components/detail/DetailComponent.vue
+++ src/components/detail/DetailComponent.vue
@@ -3,7 +3,7 @@
     <div>
       <img :src="img_backdrop + detail.backdrop_path" class="w-full lg:h-img-detail" alt="">
     </div>
-    <div class="container mx-auto">
+    <section class="container mx-auto">
       <div class="grid-cols-3 my-10 md:grid">
         <div>
           <img :src="img_poster + detail.poster_path" class="w-10/12 h-full px-2 mx-auto rounded-lg" alt="">
@@ -26,16 +26,40 @@
           <p class="text-lg text-justify">{{ detail.overview }}</p>
         </div>
       </div>
-    </div>
-    <div class="py-20 my-20 bg-black" v-show="video != ''">
+    </section>
+    <section class="py-20 my-20 bg-black" v-show="video != ''">
       <iframe class="mx-auto video md:video-md lg:video-lg" :src="video_url + video">
       </iframe>
-    </div>
+    </section>
+
+    <!-- actors Headliners -->
+
+    <section class="container mx-auto">
+      <h2 class="my-20 text-5xl text-center">Têtes d'affiche</h2>
+
+      <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3">
+
+        <div v-for="cast in casts" :key="cast.id" v-show="cast.profile_path">
+          <div>
+            <div class="w-56 mx-auto my-5 shadow-xl rounded-2xl">
+              <img :src="img_poster + cast.profile_path" class="w-56 h-80 rounded-2xl" alt="">
+
+              <div class="absolute top-0 w-full h-full p-2 opacity-0 rounded-xl hover:opacity-100">
+                <div class="p-1 rounded bg-black/70 top-44">
+                  <p class="text-2xl">{{ cast.name }}</p>
+                  <p class="text-2xl text-yellow-500">{{ cast.character }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
 <script>
 export default {
-  props: ['media', 'detail', 'img_backdrop', 'img_poster', 'video', 'video_url'],
+  props: ['media', 'detail', 'img_backdrop', 'img_poster', 'video', 'video_url', 'casts'],
 }
 </script>
